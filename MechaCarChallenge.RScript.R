cdd646487673d060251cf746c263dc88f752cc4a
alamgituoft <81877387+alamgituoft@users.noreply.github.com>
2021-08-06 22:47:32 -0400

    Add files via upload
    Challenge 1

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -1,1 +1,4 @@
-()
+library(dplyr)
+Mecha_df <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=Mecha_df)
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=Mecha_df))
